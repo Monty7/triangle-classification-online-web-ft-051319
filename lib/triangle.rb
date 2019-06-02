381fe0fe14746e659f8c41a03272fe6a784ca162
M Banks <montensia1@outlook.com>
2019-06-02 23:51:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
   end
   
   def kind 
-    if length1 <= 0 && length2 <= 0 && length3 < 0
+    if length1 <= 0 && length2 <= 0 && length3 <= 0
       raise TriangleError
     elsif length1 == length2 && length1 == length3
       :equilateral
