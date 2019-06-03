c71aad36c3230f926692e98bdea49f44f82c25fc
M Banks <montensia1@outlook.com>
2019-06-03 00:02:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,9 @@ class Triangle
     if length1 <= 0 || length2 <= 0 || length3 <= 0 || length1 + length2 < length3 || length1 + length3 <= length2 || length2 + length3 < length1
       begin
         raise TriangleError
-        rescue TriangleError
+        rescue TriangleError => error 
+        
+        
     elsif length1 == length2 && length1 == length3
       :equilateral
     elsif length2 == length3 || length1 == length3 || length1 == length2
