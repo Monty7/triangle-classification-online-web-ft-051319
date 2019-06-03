32684cd87d6576ff1b541d3ca51d8fe3841cf0fd
M Banks <montensia1@outlook.com>
2019-06-03 00:22:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,13 +13,14 @@ class Triangle
         raise TriangleError
       rescue TriangleError => error 
           puts error.message
-      end
+      
     elsif length1 == length2 && length1 == length3
       :equilateral
     elsif length2 == length3 || length1 == length3 || length1 == length2
       :isosceles
     elsif length1 != length2 && length1 != length3
       :scalene
+    end
     end
   end
   
