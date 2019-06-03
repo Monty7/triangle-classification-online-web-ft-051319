67529535675902d0242a2e037af2b08ebff027da
M Banks <montensia1@outlook.com>
2019-06-03 00:33:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,7 @@ class Triangle
     
     if length1 <= 0 || length2 <= 0 || length3 <= 0 || length1 + length2 < length3 || length1 + length3 <= length2 || length2 + length3 < length1
       begin
+      puts "I am here"
         raise TriangleError
       rescue TriangleError => error 
           puts error.message
